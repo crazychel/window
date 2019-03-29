74ce3361991a3ada024a14d445f498402a9327a1
crazychel <48129985+crazychel@users.noreply.github.com>
2019-03-29 13:26:03 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+require('formdata-polyfill');
+window.addEventListener('DOMContentLoaded', function () {
+
+  'use srict';
+  let calc = require('./parts/calc.js'),
+    form = require('./parts/form.js'),
+    slider = require('./parts/slider.js'),
+    tabs = require('./parts/tabs.js'),
+    timer = require('./parts/timer.js'),
+    modal = require('./parts/modal.js');
+
+  calc();
+  form();
+  slider();
+  tabs();
+  timer();
+  modal();
+});
+
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+  console.info('polyfill for IE11');
+  NodeList.prototype.forEach = function (callback, thisArg) {
+    thisArg = thisArg || window;
+    for (var i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
+  };
+}
